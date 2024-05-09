f809ceb97ff8cf3a2fe026d0a91e3bcb399cc746
leonlivy <wl13@ualberta>
2024-05-09 15:00:16 -0600

    refactor form

--- assign4/taskmanager/views.py
+++ assign4/taskmanager/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
+from .forms import TaskForm
 
 
 #CRUD operations:
@@ -7,7 +8,6 @@ from .models import Task
 def task_list(request):
     tasks = Task.objects.all()
     tasks.order_by('-priority') #looked from stack social flow. - from hgihest to lowest. 
-    tasks.save()
     return render(request, 'taskmanager/task_list.html', {'tasks': tasks})
 
 def task_detail(request, pk):
@@ -16,32 +16,23 @@ def task_detail(request, pk):
 
 def task_create(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        priority = request.POST['priority']
-        due_date = request.POST['due_date']
-        status = request.POST['status']
-        task = Task.objects.create(title=title, description=description, priority=priority, due_date=due_date, status=status)
-        return render(request, 'taskmanager/task_detail.html', {'task': task})
-    return render(request, 'taskmanager/task_form.html')
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save()
+            return render(request, 'taskmanager/task_detail.html', {'task': task})
+    else:
+        form = TaskForm()
+    return render(request, 'taskmanager/task_form.html', {'form': form})
 
 def task_update(request, pk):
     task = Task.objects.get(id=pk)
     if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        priority = request.POST['priority']
-        due_date = request.POST['due_date']
-        status = request.POST['status']
-        task.title = title
-        task.description = description
-        task.priority = priority
-        task.due_date = due_date
-        task.status = status
-        task.save()
-        return render(request, 'taskmanager/task_detail.html', {'task': task})
-    return render(request, 'taskmanager/task_form.html', {'task': task})
-
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return render(request, 'taskmanager/task_detail.html', {'task': task})
+    else:
+        form = TaskForm(instance=task)
 def task_delete(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
