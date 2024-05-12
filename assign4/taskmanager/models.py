de5874be6d3e88fa2d94a3327f0e739b864b01ff
wei <wl13@ualberta.ca>
2024-05-12 13:07:12 -0600

    creates size choice and test

--- assign4/taskmanager/models.py
+++ assign4/taskmanager/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Task(models.Model):
+    TASK_STATUS = [
+        ('To Do', 'To Do'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done')
+    ]
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     priority = models.IntegerField()
     due_date = models.DateField()
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=20,choices = TASK_STATUS, default='To Do')
 
 
-# Create your models here.
+
